b96d9359e252749add98e0af7dda2faf79cdb4b8
Liam Pribis <jackpribis@gmail.com>
2021-07-21 00:26:32 +1200

    rm output files

--- config.h
+++ config.h
@@ -64,7 +64,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	// { MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = +1 } },
