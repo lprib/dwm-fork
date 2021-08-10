27986e91903d0f7073bcb130048a7d6722e946bf
Liam Pribis <jackpribis@gmail.com>
2021-08-10 20:41:03 +1200

    vertical layout stack for vertical monitor

--- config.h
+++ config.h
@@ -55,8 +55,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "|||",      bstack },
 	{ "[M]",      monocle },
+	{ "<><",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -97,9 +98,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ControlMask|ShiftMask, XK_space,  setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ControlMask|ShiftMask, XK_space,  setlayout,      {.v = &layouts[3]} },
+
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
